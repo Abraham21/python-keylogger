0d34cba4dd524eec1b035e665ee96792a0438522
Abraham Yepremian <abeyepremian@gmail.com>
2018-10-06 10:40:25 -0700

    Added user providing input for maximum number of logs to fetch

--- retriever.py
+++ retriever.py
@@ -10,7 +10,7 @@ db = client.get_database()
 logs = db.logs
 
 # maximum number of listing to display from db
-listings_limit = 3
+listings_limit = int(input("Maximum number of logs to fetch?"))
 # cursor of db objects with most recent objects first
 cursor = logs.find().sort([('_id', -1)]).limit(listings_limit)
 for log in cursor:

faae497adc27e48def50012470d116d96bac99c1
Abraham Yepremian <abeyepremian@gmail.com>
2018-10-06 19:13:54 -0700

    Update retriever.py

--- retriever.py
+++ retriever.py
@@ -10,8 +10,9 @@ db = client.get_database()
 logs = db.logs
 
 # maximum number of listing to display from db
-listings_limit = int(input("Maximum number of logs to fetch?"))
+listings_limit = int(input("Maximum number of logs to fetch? "))
 # cursor of db objects with most recent objects first
 cursor = logs.find().sort([('_id', -1)]).limit(listings_limit)
+print("\n")
 for log in cursor:
     print(log["contents"] + "\n")
